ba89fd20ce0c17414d1b58e22257cf0c63a10558
Keita-N <keita.nagatomi@gmail.com>
2014-06-21 17:28:39 +0900

    Event perticipation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,8 @@ class EventsController < ApplicationController
 
 	def show
 		@event = Event.find(params[:id])
+		@tickets = @event.tickets.includes(:user).order(:created_at)
+
 	end
 
 	def index
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,7 @@ class Event < ActiveRecord::Base
 	validates :end_time, presence: true
 	validate :start_time_should_be_before_end_time
 
+	has_many :tickets
 	belongs_to :owner, class_name: 'User'
 
 	def created_by?(user)
